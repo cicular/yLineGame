df40af7392eead820c8ef273803e7b80d318fa44
cicular <circularconstant2015@gmail.com>
2022-07-17 03:25:32 +0900

    Modelのフィールド作成。PostgreSQLでマイグレーション実行。

--- yLineGameProject/settings.py
+++ yLineGameProject/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'yLineGameProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'yamanote_line_game_db',
+         'USER': 'djangouser',
+         'PASSWORD': 'djangopassword',
+         'HOST': 'localhost',
+         'PORT': '5432',
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- y_line_game_app/models.py
+++ y_line_game_app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+# python manage.py makemigrations y_line_game_app --name add_theme
+# python manage.py migrate y_line_game_app
+
+
+class Theme(models.Model):
+    # 一意に割り振られるID
+    theme_id = models.AutoField(primary_key=True)
+    # お題の題名
+    theme_title = models.CharField(max_length=200)
+    # お題の内容
+    # PostgreSQLの最大文字数は1GB（=10485760）。
+    theme_contents = models.TextField()
+    entered_contents = models.TextField(null=True)
+    user_id = models.IntegerField()
+    # お題の登録日時
+    registration_time = models.DateTimeField(default=timezone.datetime.now)
+    # お題の更新日時
+    update_time = models.DateTimeField(null=True)
+    # 最終プレイ時間
+    last_play_time = models.DateTimeField(null=True)
+    # プレイ回数
+    num_of_plays = models.IntegerField()
+    num_of_contents = models.IntegerField()
+    # 最高記録
+    best_record = models.IntegerField(null=True)
+    # 公開フラグ
+    public_flg = models.CharField(max_length=1)
+    # 削除フラグ
+    delete_flg = models.CharField(max_length=1)
