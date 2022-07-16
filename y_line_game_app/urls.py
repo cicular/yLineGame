7e8ea87742ae37f4db5fb6c28d06a5449da25a62
cicular <circularconstant2015@gmail.com>
2022-07-16 19:51:50 +0900

    DTLの基礎（リスト、辞書を渡してTemplateで表示）

--- y_line_game_app/urls.py
+++ y_line_game_app/urls.py
@@ -6,5 +6,8 @@ app_name = 'y_line_game_app'
 
 
 urlpatterns = [
+    # 127.0.0.1:8000/y_line_game_app/hello
     path('hello', views.index, name='index'),
+    # http://127.0.0.1:8000/y_line_game_app/home
+    path('home', views.home, name='home'),
 ]
--- y_line_game_app/views.py
+++ y_line_game_app/views.py
@@ -10,3 +10,17 @@ from django.http import HttpResponse
 def index(request):
     val = 'Hello World!!'
     return render(request, 'index.html', context={'value': val})
+
+
+def home(request):
+    my_name = 'Test Taro'
+    favorite_fruits = ['A', 'B', 'C']
+    my_info = {
+        'name': 'Taro',
+        'age': 25
+    }
+    return render(request, 'home.html', context={
+        'my_name': my_name,
+        'favorite_fruits': favorite_fruits,
+        'my_info': my_info
+    })
