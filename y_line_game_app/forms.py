a9db4ca04bed93ca8e00cc2513ee41733d604915
cicular <circularconstant2015@gmail.com>
2022-07-17 18:03:44 +0900

    Formのフィールドをテキストエリア化、css適用。

--- y_line_game_app/forms.py
+++ y_line_game_app/forms.py
@@ -2,5 +2,8 @@ from django import forms
 
 
 class ThemeInfo(forms.Form):
-    theme_title = forms.CharField()
-    theme_contents = forms.CharField()
+    theme_title = forms.CharField(label='お題のタイトル', max_length=40)
+    theme_contents = forms.CharField(label='お題の内容',
+                                     max_length=30000,
+                                     widget=forms.Textarea(attrs={'class': 'content', 'cols': '80', 'rows': '10'}),
+                                     )
