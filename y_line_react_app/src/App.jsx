fc59ee147cb9926e108d3f9a84c22aaef6f523f3
cicular <circularconstant2015@gmail.com>
2022-07-18 21:07:45 +0900

    React router domをインストール（npm install react-router-dom）。BrowserRouterを追加。

--- y_line_react_app/src/App.jsx
+++ y_line_react_app/src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Register from './register';
 import List from './list';
 import Play from './play';
@@ -7,12 +9,21 @@ import './style.css';
 
 const App = () => {
   return (
+    // 「BrowserRouter」は、Reactプロジェクトの中で一度しか使えない。
+    <BrowserRouter>
+    <Routes>
+    <Route path="/register" element={<Register />} />
+    <Route path="/list" element={<List />} />
+    <Route path="/play" element={ <Play />} />
+
+    </Routes>
     <div className="App">
-      <header>山手線ゲーム</header>
-      <Register />
+      {/* <header>山手線ゲーム</header> */}
+      {/* <Register />
       <Play />
-      <List />
+      <List /> */}
     </div>
+    </BrowserRouter>
   );
 }
 
