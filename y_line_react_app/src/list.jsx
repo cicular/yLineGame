d07fdbbc555595b64326e2983c338983d3273dd4
cicular <circularconstant2015@gmail.com>
2022-07-25 19:27:16 +0900

    下記サイトを参考にaxiosを使い、JSONの取得と表示。 https://omathin.com/react-api/#react%E3%81%AE%E9%96%8B%E7%99%BA%E7%92%B0%E5%A2%83%E4%BD%9C%E6%88%90

--- y_line_react_app/src/list.jsx
+++ y_line_react_app/src/list.jsx
@@ -1,15 +1,47 @@
+import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import Header from "./header";
 import Footer from "./footer";
+// import Register from './register';
+
+let themes;
+let url = 'https://jsonplaceholder.typicode.com/posts'
+    // 'http://127.0.0.1:8000/y_line_game_app/theme'
 
 const List = () => {
+  const [themes, setThemes] = useState([]);
+  // const [themes, setThemes] = useState(null);
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setThemes(response.data);
+      // const themes = response.data;
+      console.log('saa')
+    });
+  }, []);
+
+    // これは表示できるけど、axios内だとundefinedになってしまう。
+    // const themes = "こんにちは"
+
+    // axios.get(url).then((response) => {
+    //   themes = response.data;
+    //   console.log(themes);
+    //   alert(themes)
+    //   });
+
+      // var data = JSON.parse(JSON.stringify(themes));
+      // console.log(data);
+
+    // if (!themes) return null;
+
     return (
       <div className="App">
       <Header />
       <Link to="/register">お題登録</Link>
         <h1>お題一覧</h1>
-        <table>
+        {/* <table>
           <thead>
             <tr>
               <th>タイトル</th>
@@ -18,7 +50,20 @@ const List = () => {
           <tbody>
 
           </tbody>
-        </table>
+        </table> */}
+        {/* <ul> */}
+        {/* {res.data} */}
+
+        <div>
+        {/* { themes[0]} */}
+        </div>
+
+        <ul>
+        {/* { themes } */}
+        {/* { themes[0]} */}
+        { themes.map(theme => <li>{theme.title}</li>)}
+      </ul>
+
         <Footer />
       </div>
     );
