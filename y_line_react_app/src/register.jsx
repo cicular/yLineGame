4c7daedac2c547c88f4f913326b078ec12ef85eb
cicular <circularconstant2015@gmail.com>
2022-08-13 17:54:54 +0900

    不要なコード削除。インデント修正。

--- y_line_react_app/src/register.jsx
+++ y_line_react_app/src/register.jsx
@@ -86,48 +86,37 @@ const Register = () => {
     setThemeContents(e.target.value)
   }
 
-  // function updatePost() {
-  //   axios
-  //     .put(`${baseURL}/1`, {
-  //       title: "Hello World!",
-  //       body: "This is an updated post."
-  //     })
-  //     .then((response) => {
-  //       setPost(response.data);
-  //     });
-  // }
+  return (
+    <div className="App">
+    <Header />
+    <Link to="/list">お題一覧</Link>
+    <Modal show={show} setShow={setShow} modal_msg={modal_msg}/>
+    <h3>お題登録</h3>
+    <div className="input_area">
+        <label>お題のタイトル</label>
+        <div>
+        {/* onChangeを書かないと、入力することができない。 */}
+        <input type="text" placeholder="山手線の駅" value={themeTitle} onChange={handleChangeTitle}/>
+        </div>
 
-    return (
-      <div className="App">
-      <Header />
-      <Link to="/list">お題一覧</Link>
-      <Modal show={show} setShow={setShow} modal_msg={modal_msg}/>
-      <h3>お題登録</h3>
-      <div className="input_area">
-          <label>お題のタイトル</label>
-          <div>
-          {/* onChangeを書かないと、入力することができない。 */}
-          <input type="text" placeholder="山手線の駅" value={themeTitle} onChange={handleChangeTitle}/>
-          </div>
+        <div>
+        <label>お題の内容</label>
+        </div>
 
-          <div>
-          <label>お題の内容</label>
-          </div>
-
-          {/* onChangeを書かないと、入力することができない。 */}
-          <textarea value={themeContents} onChange={handleChangeContents} rows="20" cols="40" placeholder="東京,有楽町,新橋,浜松町,田町,品川,大崎,五反田,目黒,恵比寿,渋谷,原宿,代々木,新宿,新大久保,高田馬場,目白,池袋,大塚,巣鴨,駒込,田端,西日暮里,日暮里,鶯谷,上野,御徒町,秋葉原,神田,高輪ゲートウェイ"></textarea>
-          
-          <div>
-            {/* <input className='button' type="submit" value="登録" onClick={createNewTheme}/> */}
-            <button onClick={createNewTheme}>登録
-              {/* 登録画面でなぜモーダルが表示されないかといえば、一覧画面への画面遷移が勝っているから。 */}
-              {/* <Link to="/list" onClick={createNewTheme}>登録</Link> */}
-            </button>
-          </div>
-      </div>
-      <Footer />
-      </div>
-    );
-  }
+        {/* onChangeを書かないと、入力することができない。 */}
+        <textarea value={themeContents} onChange={handleChangeContents} rows="20" cols="40" placeholder="東京,有楽町,新橋,浜松町,田町,品川,大崎,五反田,目黒,恵比寿,渋谷,原宿,代々木,新宿,新大久保,高田馬場,目白,池袋,大塚,巣鴨,駒込,田端,西日暮里,日暮里,鶯谷,上野,御徒町,秋葉原,神田,高輪ゲートウェイ"></textarea>
+        
+        <div>
+          {/* <input className='button' type="submit" value="登録" onClick={createNewTheme}/> */}
+          <button onClick={createNewTheme}>登録
+            {/* 登録画面でなぜモーダルが表示されないかといえば、一覧画面への画面遷移が勝っているから。 */}
+            {/* <Link to="/list" onClick={createNewTheme}>登録</Link> */}
+          </button>
+        </div>
+    </div>
+    <Footer />
+    </div>
+  );
+}
   
-  export default Register;
+export default Register;
